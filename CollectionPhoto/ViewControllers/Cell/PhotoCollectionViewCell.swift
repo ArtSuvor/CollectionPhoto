2faa52ab98e545fa8cf63a0d834e0aa2c22111f5
HacPaTbb <85563543+HacPaTbb@users.noreply.github.com>
2021-10-23 02:00:05 +0300

    Добавил модель ячейки.

---
+++ CollectionPhoto/ViewControllers/Cell/PhotoCollectionViewCell.swift
@@ -0,0 +1,85 @@
+//
+//  Photo.swift
+//  CollectionPhoto
+//
+//  Created by Art on 23.10.2021.
+//
+
+import UIKit
+import SDWebImage
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+//MARK: - Properties
+    static let reuseId = "PhotoCollectionViewCell"
+    
+    override var isSelected: Bool {
+        didSet {
+            updateSelectedState()
+        }
+    }
+    
+    var photo: Photo! {
+        didSet {
+            let photoUrl = photo.urls["regular"]
+        }
+    }
+    
+//MARK: - UI elements
+    private let checkmark: UIImageView = {
+        let image = UIImage(systemName: "checkmark.circle.fill")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.alpha = 0
+        return imageView
+    }()
+    
+    private let photoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor = .systemGray3
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+//MARK: - Life cycle
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubviews()
+        setupConstraints()
+        updateSelectedState()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+//MARK: - Override func
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        photoImageView.image = nil
+    }
+    
+//MARK: - Functions
+    private func updateSelectedState() {
+        photoImageView.alpha = isSelected ? 0.7 : 1
+        checkmark.alpha = isSelected ? 1 : 0
+    }
+    
+    private func setupSubviews() {
+        addSubview(photoImageView)
+        photoImageView.addSubview(checkmark)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            photoImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor)])
+        
+        NSLayoutConstraint.activate([
+            checkmark.trailingAnchor.constraint(equalTo: photoImageView.trailingAnchor, constant: -10),
+            checkmark.bottomAnchor.constraint(equalTo: photoImageView.bottomAnchor, constant: -10)])
+    }
+}
--- CollectionPhoto/ViewControllers/PhotoCollectionViewController.swift
+++ CollectionPhoto/ViewControllers/PhotoCollectionViewController.swift
@@ -10,13 +10,15 @@ import UIKit
 class PhotoCollectionViewController: UICollectionViewController {
     
 //MARK: - Service
-    private let network = NetworkService()
+    private let network = NetworkDataFetcher()
     
 //MARK: - Properties
     private let cellId = "PhotoCollectionViewCell"
     private let titleCollectionView = "Photo"
+    private var timer = Timer()
+    private var photos = [Photo]()
     
-//MARK: - UI
+//MARK: - UI elements
     private lazy var addBarButtonItem: UIBarButtonItem = {
         UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addBarButtonTapped))
     }()
@@ -66,20 +68,27 @@ class PhotoCollectionViewController: UICollectionViewController {
     }
     
 //MARK: - Collection Methods
-//    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-//        5
-//    }
-//
-//    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
-//        cell.backgroundColor = .red
-//        return cell
-//    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        photos.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        
+        return cell
+    }
 }
 
 //MARK: - Extension SearchBar
 extension PhotoCollectionViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         
+        timer = Timer.scheduledTimer(withTimeInterval: 0.5, repeats: false, block: { _ in
+            self.network.fetchImage(searchTerm: searchText) {[weak self] searchResults in
+                guard let fetchPhotos = searchResults,
+                      let self = self else { return }
+                self.photos = fetchPhotos.results
+            }
+        })
     }
 }
